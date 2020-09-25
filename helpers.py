3e53c67e04775d484d97d48fe6f4b52baebcc248
Sergei Volodin <sergei.volodin@epfl.ch>
2020-09-25 19:34:23 +0200

    Add sparse matrix env

--- helpers.py
+++ helpers.py
@@ -8,6 +8,31 @@ import gin
 from vectorincrement.observation_encoder import ObservationScaleWrapper
 
 
+def rescale(x, min_value, max_value):
+    """Rescale x to be in [-1, 1] assuming values in [min_value, max_value]."""
+    x = np.copy(x)
+    x[x < min_value] = min_value
+    x[x > max_value] = max_value
+    x = (x - min_value) / (max_value - min_value)
+    x = 2 * (x - 0.5)
+    return x
+
+
+def vec_heatmap(vec, min_value=-1, max_value=1, sq_side=10):
+    """Show heatmap for the vector."""
+    vec = vec.flatten()
+    n = vec.shape[0]
+    vec_rescaled = rescale(vec, min_value, max_value)
+    obs = np.zeros((sq_side, sq_side * n, 3))
+    for i in range(n):
+        square = obs[:, i * sq_side:(i + 1) * sq_side]
+        val = vec_rescaled[i]
+        if val > 0:
+            square[:, :, 1] = vec_rescaled[i]
+        elif val < 0:
+            square[:, :, 0] = -vec_rescaled[i]
+    return obs
+
 @gin.configurable
 def load_env(env_name, time_limit=None, obs_scaler=None, wrappers=None, **kwargs):
     """Load an environment, configurable via gin."""
@@ -34,18 +59,24 @@ class EnvDataCollector(Wrapper):
         self.metadata = self.env.metadata
         self.rollouts = []
         self.current_rollout = []
+        self.steps = 0
         super(EnvDataCollector, self).__init__(env)
 
     def step(self, action):
         obs, rew, done, info = self.env.step(action)
+        self.steps += 1
         self.current_rollout.append({'observation': obs, 'reward': rew, 'done': done,
                                      'info': info, 'action': action})
         return (obs, rew, done, info)
 
-    def reset(self, **kwargs):
+    def flush(self):
         if self.current_rollout:
             self.rollouts.append(self.current_rollout)
             self.current_rollout = []
+
+    def reset(self, **kwargs):
+        self.steps += 1
+        self.flush()
         obs = self.env.reset(**kwargs)
         self.current_rollout.append({'observation': obs})
         return obs
@@ -83,7 +114,7 @@ class np_random_seed(object):
 def with_fixed_seed(fcn, seed=42, **kwargs):
     """Call function with np.random.seed fixed."""
 
-    with np_fixed_seed(seed=seed):
+    with np_random_seed(seed=seed):
         # computing the function
         result = fcn(**kwargs)
 
