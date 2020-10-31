d827fd7f68e0aa02c8e1494098aa04333d4684ca
Sergei Volodin <sergei.volodin@epfl.ch>
2020-11-01 00:46:26 +0100

    Infer module path in tests.sh

--- tests.sh
+++ tests.sh
@@ -15,6 +15,7 @@ fi
 # script directory
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+MODULE_PATH=$(python -c "import sparse_causal_model_learner_rl; print(sparse_causal_model_learner_rl.__path__[0])")
 
 echo "Running tests..."
 pytest causal_util sparse_causal_model_learner_rl vectorincrement keychest gin_tune
@@ -22,7 +23,7 @@ pytest causal_util sparse_causal_model_learner_rl vectorincrement keychest gin_t
 rm -rf $DIR/results/test_ve5
 
 echo "Running training with gin/sacred/tune"
-python -m sparse_causal_model_learner_rl.learner --config $DIR/sparse_causal_model_learner_rl/configs/test.gin --config $DIR/vectorincrement/config/ve5.gin
+python -m sparse_causal_model_learner_rl.learner --config $MODULE_PATH/configs/test.gin --config $MODULE_PATH/../vectorincrement/config/ve5.gin
 
 rm -rf $DIR/results/test_ve5
 
