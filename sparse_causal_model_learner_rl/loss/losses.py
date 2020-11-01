b74bb918f84eb3e6776475483effd681ecb1d31e
Sergei Volodin <sergei.volodin@epfl.ch>
2020-11-01 04:11:00 +0100

    Add inverse M and inverse D reconstruction losses

--- sparse_causal_model_learner_rl/loss/losses.py
+++ sparse_causal_model_learner_rl/loss/losses.py
@@ -21,6 +21,24 @@ def reconstruction_loss_norm(reconstructor, config, rn_threshold=100, **kwargs):
         regularization_loss = torch.from_numpy(np.array(rn_threshold))
     return regularization_loss
 
+@gin.configurable
+def reconstruction_loss_inverse_encoder(encoder, rn_threshold, **kwargs):
+    regularization_loss = 0
+    for param in encoder.parameters():
+        regularization_loss += torch.sum(torch.square(torch.pinverse(param)))
+    if regularization_loss < rn_threshold:
+        regularization_loss = torch.from_numpy(np.array(rn_threshold))
+    return regularization_loss
+
+@gin.configurable
+def reconstruction_loss_inverse_model(model, rn_threshold, **kwargs):
+    regularization_loss = 0
+    for param in model.parameters():
+        regularization_loss += torch.sum(torch.square(torch.pinverse(param)))
+    if regularization_loss < rn_threshold:
+        regularization_loss = torch.from_numpy(np.array(rn_threshold))
+    return regularization_loss
+
 
 @gin.configurable
 def fit_loss(obs_x, obs_y, action_x, decoder, model, **kwargs):
