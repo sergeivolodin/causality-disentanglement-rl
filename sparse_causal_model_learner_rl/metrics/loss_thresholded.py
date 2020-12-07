841392becd2642387231874e52eba4a5cea9f9f4
Sergei Volodin <sergei.volodin.ch@gmail.com>
2020-12-07 06:12:32 +0100

    Use computed threshold in loss_thresholded and nnz

--- sparse_causal_model_learner_rl/metrics/loss_thresholded.py
+++ sparse_causal_model_learner_rl/metrics/loss_thresholded.py
@@ -1,4 +1,5 @@
 from causal_util import WeightRestorer
+from sparse_causal_model_learner_rl.metrics.graph_threshold import threshold_action, threshold_features
 from torch import nn
 import torch
 import gin
@@ -8,23 +9,22 @@ def loss_thresholded(loss, trainables, model: nn.Module, context, eps=1e-3,
                      eps_out=1e-15, delta=False, **kwargs):
     """Compute a loss when models weights are thresholded."""
 
-    def threshold(param):
-        param[torch.abs(param) < eps] = eps_out
+    def threshold(param, val=eps):
+        param[torch.abs(param) < val] = eps_out
         return param
 
-    if delta:
-        value_orig = loss(**context).item()
+    value_orig = loss(**context).item() if delta else 0
 
     with WeightRestorer(models=list(trainables.values())):
+        thresholds = {'fc_features.weight': threshold_features(**context),
+                      'fc_action.weight': threshold_action(**context)}
+
         dct = model.state_dict()
-        dct = {x: threshold(y) for x, y in dct.items()}
+        dct = {x: threshold(y, thresholds.get(x, eps)) for x, y in dct.items()}
         model.load_state_dict(dct)
 
         value = loss(**context).item()
 
-    if delta:
-        result = value - value_orig
-    else:
-        result = value
+    result = value - value_orig
 
     return result
--- sparse_causal_model_learner_rl/metrics/nnz.py
+++ sparse_causal_model_learner_rl/metrics/nnz.py
@@ -1,10 +1,14 @@
 import gin
 import numpy as np
+from sparse_causal_model_learner_rl.metrics.graph_threshold import threshold_features, threshold_action
 
 
 @gin.configurable
 def nnz(model, eps=1e-3, **kwargs):
     val = 0
-    for p in model.parameters():
-        val += np.sum(np.abs(p.detach().cpu().numpy()) > eps)
+    thresholds = {'fc_features.weight': threshold_features(model),
+                  'fc_action.weight': threshold_action(model)}
+
+    for n, p in model.named_parameters():
+        val += np.sum(np.abs(p.detach().cpu().numpy()) > thresholds.get(n, eps))
     return val
