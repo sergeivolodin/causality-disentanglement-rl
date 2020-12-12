ade95e43a7cf6f1bc4953dc190b935c454167b1a
Sergei Volodin <sergei.volodin.ch@gmail.com>
2020-12-12 02:47:35 +0000

    Compute |inv(M)|_1 as well to stabilize training

--- sparse_causal_model_learner_rl/loss/losses.py
+++ sparse_causal_model_learner_rl/loss/losses.py
@@ -92,7 +92,7 @@ def sparsity_uniform(tensors, ord, maxval=100.):
     # maxval_torch = torch.abs(torch.tensor(torch.from_numpy(np.array(maxval, dtype=np.float32)), requires_grad=False))
     for param in tensors:
         regularization_loss += torch.norm(param.flatten(), p=ord) #torch.min(maxval_torch, torch.norm(param.flatten(), p=ord))
-    return regularization_loss
+    return regularization_loss / len(tensors)
 
 @gin.configurable
 def sparsity_per_tensor(tensors, ord, maxval=100.):
@@ -116,8 +116,8 @@ def sparsity_per_tensor(tensors, ord, maxval=100.):
 @gin.configurable
 def sparsity_loss_linreg(obs_x, obs_y, action_x, decoder, fcn=sparsity_uniform, ord=1, **kwargs):
     Mf, Ma = MfMa(obs_x, obs_y, action_x, decoder)
-    return fcn(tensors=[Mf, Ma], ord=ord)
-    # return sparsity_uniform([Mf, Ma], ord=ord)
+    #return fcn(tensors=[Mf, Ma, torch.pinverse(Mf), torch.pinverse(Ma)], ord=ord)
+    return sparsity_uniform([Mf, Ma, torch.inverse(Mf), torch.inverse(Ma)], ord=ord)
 
 @gin.configurable
 def sparsity_loss(model, ord=1, **kwargs):
