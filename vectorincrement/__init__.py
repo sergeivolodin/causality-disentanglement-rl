e50cfad4058465c45508f2c997e31e3536e1817b
Sergei Volodin <sergei.volodin.ch@gmail.com>
2021-01-23 03:28:07 +0100

    Add line run

--- vectorincrement/__init__.py
+++ vectorincrement/__init__.py
@@ -1,8 +1,15 @@
 import gym
 
 from vectorincrement.vectorincrementenv import VectorIncrementEnvironment
+from vectorincrement.line import LineEnvironment
+
 
 gym.envs.register(
     id='VectorIncrement-v0',
     entry_point=VectorIncrementEnvironment,
+)
+
+gym.envs.register(
+    id='Line-v0',
+    entry_point=LineEnvironment,
 )
