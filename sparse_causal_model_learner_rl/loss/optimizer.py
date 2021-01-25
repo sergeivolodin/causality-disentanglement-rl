5a3437c4c938f5a4a1402407bea8d710fe5f08c1
Sergei Volodin <sergei.volodin.ch@gmail.com>
2021-01-25 04:00:38 +0100

    Add scheduler to config

--- sparse_causal_model_learner_rl/loss/optimizer.py
+++ sparse_causal_model_learner_rl/loss/optimizer.py
@@ -5,3 +5,7 @@ import torch
 @gin.configurable
 def Optimizer(name, **kwargs):
     return getattr(torch.optim, name)(**kwargs)
+
+@gin.configurable
+def Scheduler(name, **kwargs):
+    return getattr(torch.optim.lr_scheduler, name)(**kwargs)
