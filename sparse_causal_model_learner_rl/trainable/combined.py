4315525c0b77af5b9b884a0a7cb2e5070f209246
Sergei Volodin <sergei.volodin.ch@gmail.com>
2021-01-31 14:50:00 +0100

    Add a test for FCCCombined

---
+++ sparse_causal_model_learner_rl/trainable/combined.py
@@ -0,0 +1,98 @@
+import os
+import numpy as np
+import torch
+from torch import nn
+import gin
+
+
+@gin.configurable
+class AbstractCombinedModel(nn.Module):
+    def __init__(self, n_models, input_dim, output_dim=1):
+        super(AbstractCombinedModel, self).__init__()
+        self.n_models = n_models
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+
+@gin.configurable
+class CombinedLinearLayer(nn.Module):
+    """Compute many linear layers of a single shape in a single pass.
+
+    Input shape: [batch_dim, in_features, n_models]
+    Output shape: [batch_dim, out_features, n_models]
+
+    Equation (for one model): y = Wx+b
+    """
+
+    def __init__(self, in_features, out_features, n_models):
+        super(CombinedLinearLayer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.n_models = n_models
+        self.weight = nn.Parameter(torch.zeros(self.out_features,
+                                               self.in_features,
+                                               self.n_models))
+        self.bias = nn.Parameter(torch.zeros(self.out_features, self.n_models))
+        self.reset_parameters()
+
+    def weight_by_model(self, idx):
+        return self.weight[:, :, idx]
+
+    def bias_by_model(self, idx):
+        return self.bias[:, idx]
+
+    def reset_parameters(self, apply_fcn=nn.Linear.reset_parameters):
+        class Resetter(nn.Module):
+            def __init__(self, w, b):
+                super(Resetter, self).__init__()
+                self.weight = w
+                self.bias = b
+
+        for m in range(self.n_models):
+            obj = Resetter(self.weight_by_model(m),
+                           self.bias_by_model(m))
+            apply_fcn(obj)
+
+    def forward(self, x):
+        w, b = self.weight, self.bias
+        x = torch.einsum('bim,oim->bom', x, w) + b.view(1, *b.shape)
+        return x
+
+
+@gin.configurable
+class FCCombinedModel(AbstractCombinedModel):
+    def __init__(self, hidden_sizes, activation_cls=nn.ReLU, **kwargs):
+        super(FCCombinedModel, self).__init__(**kwargs)
+        self.hidden_sizes = hidden_sizes
+        if callable(activation_cls):
+            self.activation = [activation_cls()] * len(self.hidden_sizes) + [None]
+        elif isinstance(activation_cls, list):
+            self.activation = [act_cls() if act_cls is not None else None
+                               for act_cls in activation_cls]
+        elif activation_cls is None:
+            self.activation = [None] * (len(self.hidden_sizes) + 1)
+        else:
+            raise NotImplementedError
+
+        assert len(self.activation) == len(self.hidden_sizes) + 1, (self.activation,
+                                                                    self.hidden_sizes)
+        self.dims = [self.input_dim] + self.hidden_sizes + [self.output_dim]
+        self.fc = []
+        for i in range(1, len(self.dims)):
+            self.fc.append(
+                CombinedLinearLayer(in_features=self.dims[i - 1], out_features=self.dims[i],
+                                    n_models=self.n_models))
+
+            # for torch to keep track of variables
+            setattr(self, f'fc%02d' % i, self.fc[-1])
+
+    def forward(self, x):
+        for i, fc in enumerate(self.fc):
+            x = fc(x)
+            if self.activation[i] is not None:
+                x = self.activation[i](x)
+        assert x.shape[1] == self.output_dim
+        assert x.shape[2] == self.n_models
+        if self.output_dim == 1:
+            x = x.view(x.shape[0], x.shape[2])
+        return x
---
+++ sparse_causal_model_learner_rl/trainable/test_combined.py
@@ -0,0 +1,69 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+from tqdm.auto import tqdm
+import torch
+from torch import nn
+import gin
+from sparse_causal_model_learner_rl.trainable.combined import FCCombinedModel
+
+use_cuda = False
+
+
+def get_model():
+    m = torch.nn.Sequential(
+        torch.nn.Linear(in_features=24, out_features=60,),
+        torch.nn.ReLU(),
+        torch.nn.Linear(in_features=60, out_features=60),
+        torch.nn.ReLU(),
+        torch.nn.Linear(in_features=60, out_features=60),
+        torch.nn.ReLU(),
+        torch.nn.Linear(in_features=60, out_features=1),
+    )
+    if use_cuda:
+        m = m.cuda()
+    return m
+
+class AllModels(nn.Module):
+    def __init__(self, n_models):
+        super(AllModels, self).__init__()
+
+        self.models = []
+        self.model_list = []
+        for m in range(n_models):
+            m_name = 'm%02d' % m
+            model = get_model()
+            setattr(self, m_name, model)
+            self.models.append(m_name)
+            self.model_list.append(model)
+
+    def forward(self, data, do_parallel=False):
+        results = [getattr(self, m)(data) for m in self.models]
+        return torch.cat(results, dim=1)
+
+def test_combined_inp_outp():
+    n_models = 25
+    data = torch.randn(1000, 24)
+    M = AllModels(n_models)
+    C = FCCombinedModel(hidden_sizes=[60, 60, 60], input_dim=24, n_models=n_models, output_dim=1)
+
+    for n, p in M.named_parameters():
+        model_n = int(n[1:3])
+        layer_id = int(n.split('.')[1]) // 2
+        is_bias = 'bias' in n
+        print(n, model_n, is_bias, layer_id)
+        target_param = C.fc[layer_id]
+        if is_bias:
+            target_param.bias.data[:, model_n] = p
+        else:
+            target_param.weight.data[:, :, model_n] = p
+
+    outC = C(data.view(-1, 24, 1).expand(-1, -1, n_models))
+    outM = M(data)
+
+    delta = (outC - outM).abs().max()
+    print(delta)
+    assert np.allclose(delta.item(), 0)
