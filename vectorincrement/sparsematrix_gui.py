b5c1024219d8f2c1fb3df6b74acf94bcf6725e57
Sergei Volodin <sergei.volodin@epfl.ch>
2021-03-04 23:07:43 +0100

    Add sm5 config

---
+++ vectorincrement/sparsematrix_gui.py
@@ -0,0 +1,17 @@
+import gin
+import gym
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+import seaborn as sns
+import vectorincrement
+
+        
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser(description="Show matrix")
+    parser.add_argument("--config", type=str, default=None, required=True)
+    args = parser.parse_args()
+    gin.parse_config_file(args.config)
+    env = gym.make('SparseMatrix-v0')
+    sns.heatmap(np.abs(env.A), vmin=0)
+    plt.show()
