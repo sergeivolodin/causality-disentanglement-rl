bfd9dd9d7de9ba7a2dd1d50ca928ca9869d06c15
Sergei Volodin <sergei.volodin.ch@gmail.com>
2021-03-09 21:38:11 +0100

    Fix import

--- sparse_causal_model_learner_rl/loss/sparsity_project.py
+++ sparse_causal_model_learner_rl/loss/sparsity_project.py
@@ -1,6 +1,8 @@
 import gin
 import torch
-from .l1_bal_projection_pytorch import project_onto_l1_ball
+import l1_ball_projection_pytorch
+l1_ball_projection_pytorch.torch = torch
+
 
 @gin.configurable
 def switch_project_l1(model, max_l1_norm=1, **kwargs):
@@ -13,8 +15,8 @@ def switch_project_l1(model, max_l1_norm=1, **kwargs):
         raise ValueError("Only support 1 parameter tensor")
 
     parameter = swp[0]
-    parameter.data = project_onto_l1_ball(x=parameter.data,
-                                          eps=max_l1_norm).detach()
+    parameter.data = l1_ball_projection_pytorch.project_onto_l1_ball(
+        x=parameter.data, eps=max_l1_norm).detach()
 
     # no gradient!
     return torch.sum(torch.abs(parameter.data)).detach()
